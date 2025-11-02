92a318c8d14c506f9e1477d02be9a7431da69bf0
Bogdan <apelcin4k.2016@gmail.com>
2025-11-02 16:10:16 +0200

    feat: Implement data extraction module

--- main.py
+++ main.py
@@ -1,33 +1,50 @@
 import os
 import sys
 from pyspark.sql import SparkSession
+from pyspark.sql.utils import AnalysisException
+from data_loader import load_data  # <--- ІМПОРТУЄМО НАШУ НОВУ ФУНКЦІЮ
+
 
 def main():
-    # Set Python executable path for both driver and workers
+    # Встановлюємо шлях до Python
     python_path = sys.executable
     os.environ['PYSPARK_PYTHON'] = python_path
     os.environ['PYSPARK_DRIVER_PYTHON'] = python_path
-    
-    # Ініціалізація SparkSession (точки входу)
+
+    # Ініціалізація Spark
     spark = SparkSession.builder \
-        .appName("Spark Setup Test") \
+        .appName("NYC Taxi Data Analysis") \
         .master("local[*]") \
         .getOrCreate()
 
     print(f"--- Успіх! Версія Spark: {spark.version} ---")
 
-    # Створення тестового DataFrame
-    data = [("Alice", 25), ("Bob", 30), ("Charlie", 35)]
-    columns = ["Name", "Age"]
+    # Шлях до файлу
+    file_path = "/Users/azzasel/Documents/2025 lab /trip_data_10.csv"
 
-    df = spark.createDataFrame(data, columns)
+    try:
+        # Викликаємо нашу функцію з модуля
+        print(f"--- Завантаження даних з {file_path} ---")
+        taxi_df = load_data(spark, file_path)
 
-    # Застосування методу .show()
-    print("Тестовий DataFrame успішно створено:")
-    df.show()
+        # Перевірка
+        print("--- Схема даних з data_loader: ---")
+        taxi_df.printSchema()
 
-    # Зупинка сесії
-    spark.stop()
+        print("--- Дані успішно завантажено: ---")
+        taxi_df.show(5)
+
+    except AnalysisException as e:
+        print(f"--- ПОМИЛКА: Файл не знайдено або шлях невірний ---")
+        print(e)
+    except Exception as e:
+        print(f"--- Виникла інша помилка: ---")
+        print(e)
+
+    finally:
+        print("--- Зупинка сесії Spark ---")
+        spark.stop()
+
 
 if __name__ == "__main__":
     main()
